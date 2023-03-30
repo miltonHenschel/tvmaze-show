cb9a50567fb4a9fd28767001a98705f93a48332b
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2023-03-30 19:27:43 +0100

    Setup test using Vitest

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/tvmaze-show/',
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.jsx',
+  },
 });
