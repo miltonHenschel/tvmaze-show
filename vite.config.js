a6cfd2eb89ddc9836ce3c3f7f6e0026b026eb20d
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2023-03-30 15:33:42 +0100

    Configuration for github deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: /air-polution-metrics-app/,
+  base: '/',
   plugins: [react()],
 });
