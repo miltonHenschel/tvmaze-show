5bc06ed833e905f1f263de96f02e10f76b0d2565
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2023-03-31 16:59:04 +0100

    Fix styles on Navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,14 +8,24 @@ function Navbar() {
   return (
     <>
       <Card>
-        <Nav className="align-items-center" style={{ height: '8vh' }}>
+        <Nav
+          className="align-items-center"
+          style={{
+            height: '8vh',
+            display: 'flex',
+            justifyContent: 'space-between',
+            backgroundColor: '#2657b3',
+            color: 'white',
+            padding: '0 2rem',
+          }}
+        >
           <Nav.Item>
-            <NavLink to="./">
+            <NavLink className="text-light" to="./">
               <BsChevronLeft />
             </NavLink>
           </Nav.Item>
           <Nav.Item className="">TV Shows</Nav.Item>
-          <Nav.Item className="d-flex flex-row">
+          <Nav.Item className="d-flex flex-row" style={{ gap: '2rem' }}>
             <Nav.Item className="">
               <BsFillMicFill />
             </Nav.Item>
@@ -25,6 +35,24 @@ function Navbar() {
           </Nav.Item>
         </Nav>
       </Card>
+      {/* <nav className={styles.main__container}>
+        <div className={styles.container}>
+          <div className={styles.me__auto}>
+            <NavLink to="./">
+              <BsChevronLeft />
+            </NavLink>
+          </div>
+          <div>Navbar</div>
+          <div className={styles.me__auto}>
+            <div>
+              <BsFillMicFill />
+            </div>
+            <div>
+              <BsGear />
+            </div>
+          </div>
+        </div>
+      </nav> */}
       <Outlet />
     </>
   );
